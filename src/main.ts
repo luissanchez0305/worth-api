e1c5ce0e892c77c6fba022ccc110ec4b001ae58d
Luis R Sanchez <luis.sanchez@esferasoluciones.com>
2022-07-27 21:47:40 -0500

    remover package-lock.json poner cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   let app = await NestFactory.create(AppModule);
   app = app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  });
+  await app.listen(process.env.PLATFORM_PORT);
 }
 bootstrap();
