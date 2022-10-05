2778b4b2fd5b3f154194aba9ba3107093aa0ceee
Luis Ricardo Sanchez <luis.sanchez@esferasoluciones.com>
2022-10-05 11:27:30 -0500

    make port default

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
