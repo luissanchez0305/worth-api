af6167b0f58c355710778686faed963a5d77b7e8
Luis Ricardo Sanchez <luis.sanchez@esferasoluciones.com>
2023-01-04 00:27:56 -0500

    change port host listeen logic

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,11 @@ async function bootstrap() {
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
-  const port = process.env.HOST_PORT || 5000;
-  const server = await app.listen(port);
-  console.log('server', server._connectionKey);
+  const server_port = process.env.YOUR_PORT || process.env.HOST_PORT || 8080;
+  const server_host = process.env.YOUR_HOST || '0.0.0.0';
+  app.listen(server_port, server_host, () => {
+    console.log('Listening on port %d', server_port);
+  });
 }
+
 bootstrap();
