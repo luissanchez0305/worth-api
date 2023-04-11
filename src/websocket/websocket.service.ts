7ade4d92457143a9e86bc487cd74a42af34bffed
Luis Ricardo Sanchez <luis.sanchez@esferasoluciones.com>
2023-04-11 11:01:51 -0500

    working with tiingo

--- src/websocket/websocket.service.ts
+++ src/websocket/websocket.service.ts
@@ -7,49 +7,58 @@ import { Signal } from 'src/signals/signals.model';
 import { SignalSymbolsService } from 'src/signalSymbol/signalSymbol.service';
 import { SignalsService } from 'src/signals/signals.service';
 import { SignalLogsService } from 'src/SignalLogs/signalLogs.service';
-import { MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-import { Server } from 'socket.io'
-@WebSocketGateway()
+import { WebSocket } from 'ws';
+
 export class WebsocketService {
+  ws: WebSocket = new WebSocket(
+    // `wss://ws.finnhub.io?token=${process.env.FINNHUB_KEY}`,
+    'wss://api.tiingo.com/crypto',
+  );
   constructor(
     private readonly signalSymbolsService: SignalSymbolsService,
     private readonly signalsService: SignalsService,
     private readonly signalLogsService: SignalLogsService,
   ) {
+    const subscribe = {
+      eventName: 'subscribe',
+      authorization: '74658269659330ee03fa822c23520b6b453325f2',
+      eventData: {
+        thresholdLevel: 5,
+      },
+    };
 
+    this.ws.on('open', function open() {
+      this.send(JSON.stringify(subscribe));
+    });
+
+    this.ws.on('message', function (event, isBinary) {
+      const message = JSON.parse(event.toString());
+
+      if (
+        message.data &&
+        message.data.length &&
+        message.data[3] === 'binance' &&
+        message.data[1] === 'btcbusd'
+      ) {
+        console.log('Message from server ', message.data, message.data[0]);
+      }
+    });
   }
 
-  server: Server;
+  async startWebsocket(symbol: string) {
+    // this.ws.on('subscribe', function (event, isBinary) {
+    this.ws.send(JSON.stringify({ type: 'subscribe', symbol }));
+    // });
 
-  @SubscribeMessage('newSignal')
-  onNewSignal(@MessageBody() signalObj: { signal: Signal }){
-    console.log("🚀 ~ file: websocket.service.ts:23 ~ WebsocketService ~ onNewSignal ~ signal", signalObj)
+    this.ws.on('message', function (event, isBinary) {
+      const message = isBinary ? event : event.toString();
+
+      console.log('Message from server ', message);
+    });
   }
 
-
-  // ws: WebSocket = null;
-  // constructor(
-  //   private readonly signalSymbolsService: SignalSymbolsService,
-  //   private readonly signalsService: SignalsService,
-  //   private readonly signalLogsService: SignalLogsService,
-  // ) {
-  //   // this.ws = new WebSocket(
-  //   //   `wss://ws.finnhub.io?token=${process.env.FINNHUB_KEY}`,
-  //   // );
-  // }
-
-  // async startWebsocket(symbol: string) {
-  //   this.ws.send(JSON.stringify({ type: 'subscribe', symbol }));
-
-  //   this.ws.on('message', function (event, isBinary) {
-  //     const message = isBinary ? event : event.toString();
-
-  //     console.log('Message from server ', message);
-  //   });
-  // }
-
-  // async stopWebsocket(symbol: string) {
-  //   console.log('stop', symbol);
-  //   this.ws.send(JSON.stringify({ type: 'unsubscribe', symbol: symbol }));
-  // }
+  async stopWebsocket(symbol: string) {
+    console.log('stop', symbol);
+    this.ws.send(JSON.stringify({ type: 'unsubscribe', symbol: symbol }));
+  }
 }
